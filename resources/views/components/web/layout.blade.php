8d17273a09167eeb749c797b806110be288a0b96
abdawyy <eyadomar.ok@gmail.com>
2026-09-25 01:53:47 +0300

    pixel code

--- resources/views/components/web/layout.blade.php
+++ resources/views/components/web/layout.blade.php
@@ -36,6 +36,24 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?v={{ filemtime(public_path('assets/css/style.css')) }}">
     <link rel="stylesheet" href="{{ asset('assets/css/storefront-theme.css') }}?v={{ filemtime(public_path('assets/css/storefront-theme.css')) }}">
     <script src="{{ asset('assets/js/storefront-theme.js') }}?v={{ filemtime(public_path('assets/js/storefront-theme.js')) }}"></script>
+
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '3262657207254492');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=3262657207254492&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body class="storefront-body">
     <a class="skip-to-content" href="#storefront-main">{{ __('web.skip_to_content') }}</a>
